d2cffb57338ed568f436e894097e72270372d6ce
Praveen Kumar <praveen9166050@gmail.com>
2024-07-18 22:58:44 +0530

    Refactored Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,11 @@ function Navbar() {
   const [toggle, setToggle] = useState(false);
   return (
     <nav className='w-full py-6 flex justify-between items-center navbar'>
-      <img src={logo} alt="hoobank"
-      className='w-[124px] h-[32px]' />
+      <img 
+        src={logo} 
+        alt="hoobank"
+        className='w-[124px] h-[32px]' 
+      />
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
           <li
@@ -21,7 +24,9 @@ function Navbar() {
         ))}
       </ul>
       <div className='sm:hidden flex flex-1 justify-end items-center'>
-        <img src={toggle ? close : menu} alt="menu"
+        <img 
+          src={toggle ? close : menu} 
+          alt="menu"
           className='w-[28px] h-[28px] object-contain'
           onClick={() => setToggle((toggle) => !toggle)} 
         />
